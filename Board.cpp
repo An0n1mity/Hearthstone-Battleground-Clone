a06137745a881de3a46b13d08bcfb77b08c9ed66
An0n1mity <an0nimity@protonmail.com>
2022-12-07 20:34:47 +0100

    Board added

---
+++ Board.cpp
@@ -0,0 +1,14 @@
+#include "Board.h"
+
+std::vector<std::shared_ptr<Card>> Board::getPlayerCards(Player *player)
+{
+    std::vector<std::shared_ptr<Card>> player_cards;
+    for (auto card : m_cards)
+    {
+        if (card->getOwner() == player)
+        {
+            player_cards.push_back(card);
+        }
+    }
+    return player_cards;
+}
